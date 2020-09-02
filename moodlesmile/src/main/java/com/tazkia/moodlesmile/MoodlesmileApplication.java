cb1300f2c887dc831eb335dec184893134ca281f
Ramadhani Indra Sawijianto <ramadhani.drasaw@gmail.com>
2020-09-02 11:44:08 +0700

    First commit

---
+++ moodlesmile/src/main/java/com/tazkia/moodlesmile/MoodlesmileApplication.java
@@ -0,0 +1,13 @@
+package com.tazkia.moodlesmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodlesmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodlesmileApplication.class, args);
+	}
+
+}
---
+++ moodlesmile/src/test/java/com/tazkia/moodlesmile/MoodlesmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tazkia.moodlesmile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodlesmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
