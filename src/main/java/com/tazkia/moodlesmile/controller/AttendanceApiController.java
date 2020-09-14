e461ef05e327d6df75f42fa1bef15da9e03f2a96
Sawrain <ramadhani.drasaw@gmail.com>
2020-09-14 10:55:02 +0700

    Add data

--- src/main/java/com/tazkia/moodlesmile/controller/AttendanceApiController.java
+++ src/main/java/com/tazkia/moodlesmile/controller/AttendanceApiController.java
@@ -6,6 +6,7 @@ import com.tazkia.moodlesmile.dao.MdlAttendanceLogDao;
 import com.tazkia.moodlesmile.dao.MdlAttendanceSessionsDao;
 import com.tazkia.moodlesmile.dto.MdlAttendanceLogDto;
 import com.tazkia.moodlesmile.entity.MdlAttendanceLog;
+import com.tazkia.moodlesmile.entity.MdlAttendanceSessions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,20 +35,35 @@ public class AttendanceApiController {
     private MdlAttendanceSessionsDao mdlAttendanceSessionsDao;
 
 
-    @GetMapping("/api/attendancelog")
+    @GetMapping("/api/session")
     @ResponseBody
     public List<MdlAttendanceLogDto> attendanceLog(@RequestParam BigInteger sessionid){
 
-    List<MdlAttendanceLog> alog =mdlAttendanceLogDao.findBySessionid(mdlAttendanceSessionsDao.findById(sessionid).get());
+    List<MdlAttendanceLog> alog = mdlAttendanceLogDao.findBySessionid(mdlAttendanceSessionsDao.findById(sessionid).get());
     List<MdlAttendanceLogDto> adto = new ArrayList<>();
 
 
         for (MdlAttendanceLog mdlAttendanceLog : alog){
         MdlAttendanceLogDto mdlAttendanceLogDto = new MdlAttendanceLogDto();
         mdlAttendanceLogDto.setId(mdlAttendanceLog.getId());
-        mdlAttendanceLogDto.setSessionid(mdlAttendanceLog.getSessionid());
-        mdlAttendanceLogDto.setStudentid(mdlAttendanceLog.getStudentid());
-        mdlAttendanceLogDto.setTakenby(mdlAttendanceLog.getTakenby());
+
+        mdlAttendanceLogDto.setStudentid(mdlAttendanceLog.getStudentid().getId());
+        mdlAttendanceLogDto.setFirstname(mdlAttendanceLog.getStudentid().getFirstname());
+        mdlAttendanceLogDto.setTakenby(mdlAttendanceLog.getTakenby().getFirstname());
+
+        mdlAttendanceLogDto.setSessionid(mdlAttendanceLog.getSessionid().getId());
+        mdlAttendanceLogDto.setSessdate(mdlAttendanceLog.getSessionid().getSessdate());
+
+        mdlAttendanceLogDto.setStatusid(mdlAttendanceLog.getStatusid().getId());
+        mdlAttendanceLogDto.setDescription(mdlAttendanceLog.getStatusid().getDescription());
+
+        mdlAttendanceLogDto.setAttendanceid(mdlAttendanceLog.getSessionid().getAttendanceid().getId());
+        mdlAttendanceLogDto.setGrade(mdlAttendanceLog.getSessionid().getAttendanceid().getGrade());
+
+
+        mdlAttendanceLogDto.setCourse(mdlAttendanceLog.getSessionid().getAttendanceid().getCourse().getId());
+        mdlAttendanceLogDto.setFullname(mdlAttendanceLog.getSessionid().getAttendanceid().getCourse().getFullname());
+
         adto.add(mdlAttendanceLogDto);
         }
 
