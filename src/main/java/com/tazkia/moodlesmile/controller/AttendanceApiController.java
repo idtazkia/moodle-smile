44424b7a6d0666a3ce6ea075e3c7da869ee86875
Sawrain <ramadhani.drasaw@gmail.com>
2020-10-06 14:06:44 +0700

    Add find mahasiswa query

--- src/main/java/com/tazkia/moodlesmile/controller/AttendanceApiController.java
+++ src/main/java/com/tazkia/moodlesmile/controller/AttendanceApiController.java
@@ -5,19 +5,15 @@ import com.tazkia.moodlesmile.dao.MdlAttendanceDao;
 import com.tazkia.moodlesmile.dao.MdlAttendanceLogDao;
 import com.tazkia.moodlesmile.dao.MdlAttendanceSessionsDao;
 import com.tazkia.moodlesmile.dao.MdlCourseDao;
-import com.tazkia.moodlesmile.dto.CourseDto;
-import com.tazkia.moodlesmile.dto.MdlAttendanceLogDto;
-import com.tazkia.moodlesmile.dto.MdlAttendanceLogIntDto;
-import com.tazkia.moodlesmile.entity.MdlAttendanceLog;
-import com.tazkia.moodlesmile.entity.MdlAttendanceSessions;
-import com.tazkia.moodlesmile.entity.MdlCourse;
+import com.tazkia.moodlesmile.dto.MdlAttendanceLogDosenDto;
+import com.tazkia.moodlesmile.dto.MdlAttendanceLogDosenIntDto;
+import com.tazkia.moodlesmile.dto.MdlAttendanceLogMahasiswaDto;
+import com.tazkia.moodlesmile.dto.MdlAttendanceLogMahasiswaIntDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,53 +36,62 @@ public class AttendanceApiController {
     private MdlCourseDao mdlCourseDao;
 
 
-    @GetMapping("/api/session")
+    @GetMapping("/api/sessiondosen")
     @ResponseBody
-    public List<MdlAttendanceLogDto> attendanceLog(){
+    public List<MdlAttendanceLogDosenDto> attendanceLogDosen(){
 
-        List<MdlAttendanceLogIntDto> alog = mdlAttendanceLogDao.findJadwalSekarang();
-        List<MdlAttendanceLogDto> adto = new ArrayList<>();
+        List<MdlAttendanceLogDosenIntDto> alog = mdlAttendanceLogDao.findJadwalSekarangDosen();
+        List<MdlAttendanceLogDosenDto> adto = new ArrayList<>();
 
 //    adto.addAll(alog);
 
 
-        for (MdlAttendanceLogIntDto mdlAttendanceLog : alog){
-        MdlAttendanceLogDto mdlAttendanceLogDto = new MdlAttendanceLogDto();
-//        mdlAttendanceLogDto.setId(mdlAttendanceLog.getId());
-//
-//        mdlAttendanceLogDto.setStudentid(mdlAttendanceLog.getStudentid().getId());
-//        mdlAttendanceLogDto.setIdnumbernim(mdlAttendanceLog.getStudentid().getIdnumber());
-//        mdlAttendanceLogDto.setFirstname(mdlAttendanceLog.getStudentid().getFirstname());
-//        mdlAttendanceLogDto.setTakenby(mdlAttendanceLog.getTakenby().getFirstname());
-//        mdlAttendanceLogDto.setEmail(mdlAttendanceLog.getStudentid().getEmail());
-//
-//        mdlAttendanceLogDto.setSessionid(mdlAttendanceLog.getSessionid().getId());
-//        mdlAttendanceLogDto.setSessdate(mdlAttendanceLog.getSessionid().getSessdate());
-//
-//        mdlAttendanceLogDto.setStatusid(mdlAttendanceLog.getStatusid().getId());
-//        mdlAttendanceLogDto.setDescription(mdlAttendanceLog.getStatusid().getDescription());
-//
-//        mdlAttendanceLogDto.setAttendanceid(mdlAttendanceLog.getSessionid().getAttendanceid().getId());
-//        mdlAttendanceLogDto.setGrade(mdlAttendanceLog.getSessionid().getAttendanceid().getGrade());
-//
-//
-//        mdlAttendanceLogDto.setCourse(mdlAttendanceLog.getSessionid().getAttendanceid().getCourse().getId());
-//        mdlAttendanceLogDto.setFullname(mdlAttendanceLog.getSessionid().getAttendanceid().getCourse().getFullname());
-//        mdlAttendanceLogDto.setShortname(mdlAttendanceLog.getSessionid().getAttendanceid().getCourse().getShortname());
-//        mdlAttendanceLogDto.setIdnumbercourse(mdlAttendanceLog.getSessionid().getAttendanceid().getCourse().getIdnumber());
+        for (MdlAttendanceLogDosenIntDto mdlAttendanceLog : alog){
+        MdlAttendanceLogDosenDto mdlAttendanceLogDosenDto = new MdlAttendanceLogDosenDto();
+
+        mdlAttendanceLogDosenDto.setIdlog(mdlAttendanceLog.getId());
+        mdlAttendanceLogDosenDto.setIdTahunAkademik(mdlAttendanceLog.getIdTahunAkademik());
+        mdlAttendanceLogDosenDto.setIdJadwal(mdlAttendanceLog.getIdJadwal());
+        mdlAttendanceLogDosenDto.setWaktuMasuk(mdlAttendanceLog.getWaktuMasuk());
+        mdlAttendanceLogDosenDto.setWaktuSelesai(mdlAttendanceLog.getWaktuSelesai());
+        mdlAttendanceLogDosenDto.setStatusPresensi(mdlAttendanceLog.getStatusPresensi());
+        mdlAttendanceLogDosenDto.setStatus(mdlAttendanceLog.getStatus());
+        mdlAttendanceLogDosenDto.setIdDosen(mdlAttendanceLog.getIdDosen());
+        mdlAttendanceLogDosenDto.setBeritaAcara(mdlAttendanceLog.getBeritaAcara());
 
 
-        mdlAttendanceLogDto.setIdlog(mdlAttendanceLog.getId());
-        mdlAttendanceLogDto.setIdTahunAkademik(mdlAttendanceLog.getIdTahunAkademik());
-        mdlAttendanceLogDto.setIdJadwal(mdlAttendanceLog.getIdJadwal());
-        mdlAttendanceLogDto.setWaktuMasuk(mdlAttendanceLog.getWaktuMasuk());
-        mdlAttendanceLogDto.setWaktuSelesai(mdlAttendanceLog.getWaktuSelesai());
-        mdlAttendanceLogDto.setStatusPresensi(mdlAttendanceLog.getStatusPresensi());
-        mdlAttendanceLogDto.setStatus(mdlAttendanceLog.getStatus());
-        mdlAttendanceLogDto.setIdDosen(mdlAttendanceLog.getIdDosen());
+        adto.add(mdlAttendanceLogDosenDto);
+        }
+
+        return adto;
+
+    }
+
+    @GetMapping("/api/sessionmahasiswa")
+    @ResponseBody
+    public List<MdlAttendanceLogMahasiswaDto> attendanceLogMahasiswa(){
+
+        List<MdlAttendanceLogMahasiswaIntDto> alog = mdlAttendanceLogDao.findJadwalSekarangMahasiswa();
+        List<MdlAttendanceLogMahasiswaDto> adto = new ArrayList<>();
+
+//    adto.addAll(alog);
 
 
-        adto.add(mdlAttendanceLogDto);
+        for (MdlAttendanceLogMahasiswaIntDto mdlAttendanceLog : alog){
+            MdlAttendanceLogMahasiswaDto mdlAttendanceLogMahasiswaDto = new MdlAttendanceLogMahasiswaDto();
+
+            mdlAttendanceLogMahasiswaDto.setIdlog(mdlAttendanceLog.getId());
+            mdlAttendanceLogMahasiswaDto.setIdTahunAkademik(mdlAttendanceLog.getIdTahunAkademik());
+            mdlAttendanceLogMahasiswaDto.setIdJadwal(mdlAttendanceLog.getIdJadwal());
+            mdlAttendanceLogMahasiswaDto.setWaktuMasuk(mdlAttendanceLog.getWaktuMasuk());
+            mdlAttendanceLogMahasiswaDto.setWaktuSelesai(mdlAttendanceLog.getWaktuSelesai());
+            mdlAttendanceLogMahasiswaDto.setStatusPresensi(mdlAttendanceLog.getStatusPresensi());
+            mdlAttendanceLogMahasiswaDto.setStatus(mdlAttendanceLog.getStatus());
+            mdlAttendanceLogMahasiswaDto.setMahasiswa(mdlAttendanceLog.getMahasiswa());
+
+
+
+            adto.add(mdlAttendanceLogMahasiswaDto);
         }
 
         return adto;
--- src/main/java/com/tazkia/moodlesmile/dao/MdlAttendanceLogDao.java
+++ src/main/java/com/tazkia/moodlesmile/dao/MdlAttendanceLogDao.java
@@ -1,10 +1,8 @@
 package com.tazkia.moodlesmile.dao;
 
-import com.tazkia.moodlesmile.dto.MdlAttendanceLogDto;
-import com.tazkia.moodlesmile.dto.MdlAttendanceLogIntDto;
+import com.tazkia.moodlesmile.dto.MdlAttendanceLogDosenIntDto;
+import com.tazkia.moodlesmile.dto.MdlAttendanceLogMahasiswaIntDto;
 import com.tazkia.moodlesmile.entity.MdlAttendanceLog;
-import com.tazkia.moodlesmile.entity.MdlAttendanceSessions;
-import com.tazkia.moodlesmile.entity.MdlCourse;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -19,18 +17,33 @@ public interface MdlAttendanceLogDao extends PagingAndSortingRepository<MdlAtten
 
 
     //find dosen
-    @Query(value = "SELECT b.id AS id, '6cf08aa7-fc62-40f3-b82d-ff04be2c8905' AS idTahunAkademik, e.shortname AS idJadwal, FROM_UNIXTIME(a.timetaken, '%d/%m/%Y %h:%m:%s') AS waktuMasuk, \n" +
-            "FROM_UNIXTIME(e.enddate, '%d/%m/%Y %h:%m:%s') AS waktuSelesai,'HADIR' AS statusPresensi, 'AKTIF' AS status, f.email AS idDosen\n" +
+    @Query(value = "SELECT b.id AS id, '6cf08aa7-fc62-40f3-b82d-ff04be2c8905' AS idTahunAkademik,e.fullname AS namaMatakuliah, e.shortname AS idJadwal, FROM_UNIXTIME(a.timetaken, '%Y-%m-%d %h:%m:%s') AS waktuMasuk, \n" +
+            "FROM_UNIXTIME(e.enddate, '%Y-%m-%d %h:%m:%s') AS waktuSelesai,'HADIR' AS statusPresensi, 'AKTIF' AS status, f.email AS idDosen, b.description AS beritaAcara\n" +
             "FROM mdl_attendance_log AS a \n" +
             "INNER JOIN mdl_attendance_sessions AS b ON a.sessionid = b.id\n" +
             "INNER JOIN mdl_attendance AS c ON b.attendanceid = c.id \n" +
             "INNER JOIN mdl_attendance_statuses AS d ON c.id = d.attendanceid AND a.statusid = d.id\n" +
             "INNER JOIN mdl_course AS e ON c.course = e.id\n" +
-            "INNER JOIN mdl_user AS f ON a.takenby = f.id\n" +
+            "INNER JOIN mdl_user AS f ON b.lasttakenby = f.id\n" +
             "INNER JOIN mdl_user AS g ON a.studentid = g.id\n" +
-            "WHERE e.fullname LIKE '%20201%'\n" +
-            "GROUP BY b.id;", nativeQuery = true)
-    List<MdlAttendanceLogIntDto> findJadwalSekarang();
+            "WHERE e.fullname LIKE '%20201%' AND LENGTH(e.shortname)= 36 \n" +
+            "GROUP BY b.id", nativeQuery = true)
+    List<MdlAttendanceLogDosenIntDto> findJadwalSekarangDosen();
+
+
+    @Query(value = "SELECT b.id AS id, '6cf08aa7-fc62-40f3-b82d-ff04be2c8905' AS idTahunAkademik,e.fullname AS namaMatakuliah, e.shortname AS idJadwal, FROM_UNIXTIME(a.timetaken, '%Y-%m-%d %h:%m:%s') AS waktuMasuk, \n" +
+            "FROM_UNIXTIME(e.enddate, '%Y-%m-%d %h:%m:%s') AS waktuSelesai,d.description AS statusPresensi, 'AKTIF' AS status, g.email AS mahasiswa\n" +
+            "FROM mdl_attendance_log AS a \n" +
+            "INNER JOIN mdl_attendance_sessions AS b ON a.sessionid = b.id\n" +
+            "INNER JOIN mdl_attendance AS c ON b.attendanceid = c.id \n" +
+            "INNER JOIN mdl_attendance_statuses AS d ON c.id = d.attendanceid AND a.statusid = d.id\n" +
+            "INNER JOIN mdl_course AS e ON c.course = e.id\n" +
+            "INNER JOIN mdl_user AS f ON b.lasttakenby = f.id\n" +
+            "INNER JOIN mdl_user AS g ON a.studentid = g.id\n" +
+            "WHERE e.fullname LIKE '%20201%' AND LENGTH(e.shortname)= 36", nativeQuery = true)
+    List<MdlAttendanceLogMahasiswaIntDto> findJadwalSekarangMahasiswa();
+
+
 
 
 }
