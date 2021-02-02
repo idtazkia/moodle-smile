1ba66692ab303bf6a9698d772c7733d7a4f1191b
Sawrain <ramadhani.drasaw@gmail.com>
2021-02-02 13:37:50 +0700

    Grade Items DTO

---
+++ src/main/java/com/tazkia/moodlesmile/dto/MdlGradeItemsDto.java
@@ -0,0 +1,20 @@
+package com.tazkia.moodlesmile.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Data
+public class MdlGradeItemsDto {
+
+    private BigInteger id;
+    private String idJadwal;
+    private String namaTugas;
+    private BigDecimal bobot;
+    private String statusA;
+    private BigInteger pertemuan;
+    private String timeCreated;
+    private String timeModified;
+
+}
---
+++ src/main/java/com/tazkia/moodlesmile/dto/MdlGradeItemsIntDto.java
@@ -0,0 +1,18 @@
+package com.tazkia.moodlesmile.dto;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public interface MdlGradeItemsIntDto {
+
+    BigInteger getId();
+    String getIdJadwal();
+    String getNamaTugas();
+    BigDecimal getBobot();
+    String getStatusA();
+    BigInteger getPertemuan();
+    String getTimeCreated();
+    String getTimeModified();
+
+
+}
